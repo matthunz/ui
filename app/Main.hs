40d1267bda48be829ab3d8b490f6763d1e02b17e
Matt Hunzinger <matthunz2@gmail.com>
2024-06-22 18:06:42 -0400

    Create Scope monad

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE GADTs #-}
+
 module Main where
 
 import qualified MyLib (someFunc)
@@ -6,3 +8,19 @@ main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
   MyLib.someFunc
+
+data Scope a where
+  PureS :: a -> Scope a
+  OnceS :: IO a -> Scope a
+  MapS :: (b -> a) -> Scope b -> Scope a
+  BindS :: Scope b -> (b -> Scope a) -> Scope a
+
+instance Functor Scope where
+  fmap = MapS
+
+instance Applicative Scope where
+  pure = PureS
+  (<*>) sf sa = BindS sf (`fmap` sa)
+
+instance Monad Scope where
+  (>>=) = BindS
